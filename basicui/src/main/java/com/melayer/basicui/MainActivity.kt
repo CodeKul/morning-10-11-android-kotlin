3d3437d59686fe0f51fb7f15829407ab24af99fe
aniruddha <aniruddha.kudalkar@gmail.com>
2018-03-20 11:26:02 +0530

    Context, Views: details of context and views

--- basicui/src/main/java/com/melayer/basicui/MainActivity.kt
+++ basicui/src/main/java/com/melayer/basicui/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.melayer.basicui
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,12 +16,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val cntx: Context = this@MainActivity
+
+        val clk : View.OnClickListener  = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+            }
+        }
+        btnOkay.setOnClickListener(clk)
+
+        val clkV2: View.OnClickListener = View.OnClickListener {
+
+        }
+
+        btnOkay.setOnClickListener(clkV2)
+
+        btnOkay.setOnClickListener(this::clicked)
+
         btnOkay.setOnClickListener {
             val mb = etMb.text.toString()
             val pass = etPass.text.toString()
-            if(mb == pass)  txtRs.text = "Success"
+            if (mb == pass) txtRs.text = "Success"
             else txtRs.text = "Fail"
         }
+
+        val vw: View = btnOkay
+
+        val lyt : LinearLayout = LinearLayout(this@MainActivity)
+
+        val cnst : ConstraintLayout = ConstraintLayout(this@MainActivity)
+    }
+    private fun clicked(vw : View) {
 
     }
 }
